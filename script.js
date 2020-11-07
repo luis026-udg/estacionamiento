ec77f1f318225a06b021e67b169c0c59311df3d9
luis026-udg <luis.luisjuan@udgvirtual.udg.mx>
2020-11-06 23:42:38 -0600

    Complete

--- script.js
+++ script.js
@@ -1,45 +1,68 @@
-function createStartArray() {
-  startArray = ["leche", "huevos", "pan", "yogurt", "frijoles", "pollo", "café"];
-  startArray.push("arroz");
-}
-function createArrayInvert() {
-  arrayInvert = startArray.slice();//Esta linea utiliza el metodo slice para crear una copia independiente de un array en JS
-  arrayInvert.reverse();
-}
-function createFinalArray() {
-  finalArray = arrayInvert.slice();//Esta linea utiliza el metodo slice para crear una copia independiente de un array en JS
-  let posicionLeche = finalArray.indexOf("leche");
-  let posicionCafe = finalArray.indexOf("café");
-  finalArray.splice(posicionLeche, 1, "leche descremada")
-  finalArray.splice(posicionCafe, 1, "café descafeinado")
-}
-function displayArrays(showArray) 
+function playGame() 
 {
-  for (var i = 0; i < showArray.length; i++) {
-    let posicion = 0;
-    let valor = 0;
-    posicion = i;
-    valor = showArray[i];
-    resultado.insertAdjacentHTML("beforeend", `<br>El elemento en la posición ${posicion} es ${valor} `);
-  }
-}
-function playGame() {
-  startArray = [];
-  arrayInvert = [];
-  finalArray = [];
-  createStartArray();
-  createArrayInvert();
-  createFinalArray();
-  console.log(finalArray);
+  
   document.getElementById("resultado").innerHTML = "<br>";
-  resultado.insertAdjacentHTML("beforeend", `<h2>Arreglo 1</h2>`);
-  displayArrays(arrayInvert);
-  resultado.insertAdjacentHTML("beforeend", `<h2>Arreglo 2</h2>`);
-  displayArrays(finalArray);
+  resultado.insertAdjacentHTML("beforeend", `<h2>Start</h2> `);
+  showElements();
+  
+  cajones.splice(lastElement, 1, "Blanco");
+  
+  resultado.insertAdjacentHTML("beforeend", `<h2>Agregar Auto Blanco</h2> `);
+  showElements();
+  let posicionElementoBusqueda = cajones.indexOf("Amarillo");
+  resultado.insertAdjacentHTML("beforeend", `<h2>Busqueda Vehículo Amarillo</h2> `);
+  resultado.insertAdjacentHTML("beforeend", `El Vehículo Amarillo se encuentra en la posicion ${posicionElementoBusqueda}  `);
+  
+  resultado.insertAdjacentHTML("beforeend", `<h2>Busqueda de un Auto mediante función FOR</h2> `);
+  
+  var colorAutoBusqueda = "Gris";
+  encontrarAuto(colorAutoBusqueda);
 }
-var startArray = [];
-var arrayInvert = [];
-var finalArray = [];
+
+
+function encontrarAuto(colorAuto) {
+  let encontrado = false;
+  let posicion;
+  for (let i = 0; i < cajones.length; i++) {
+    if (cajones[i] == colorAuto) {
+      posicion = i;
+      encontrado = true;
+      break;
+    }
+  }
+  if (encontrado == true) {
+    resultado.insertAdjacentHTML("beforeend", `<br>El carro de color ${colorAuto} esta en la posición ${posicion}. `);
+  }
+  else {
+    resultado.insertAdjacentHTML("beforeend", `<br>El carro de color ${colorAuto} no esta en el estacionamiento`);
+  }
+  
+}
+function showElements() {
+  
+  carros_estacionados = cajones.length;
+  for (let i = 0; i < cajones.length; i++) {
+    let valor = cajones[i];
+    let posicion = i;
+    if (cajones[i] == "vacio") {
+      carros_estacionados--;
+      resultado.insertAdjacentHTML("beforeend", `<br><strong>El elemento en la posición ${posicion} esta Vacio </strong>`);
+    }
+    else {
+      resultado.insertAdjacentHTML("beforeend", `<br>El elemento en la posición ${posicion} es ${valor} `);
+      
+    }
+    console.log(posicion);
+  }
+  resultado.insertAdjacentHTML("beforeend", `<br><br>En este momento hay ${carros_estacionados} carros en el estacionamiento `);
+  
+  
+}
 var resultado = document.getElementById("resultado");
 var b = document.getElementById("play");
 b.addEventListener("click", playGame);
+var cajones = [];
+cajones = ["Rojo", "Gris", "Azul", "Amarillo", "Verde", "vacio"];
+console.log(cajones);
+var carros_estacionados = cajones.length;
+var lastElement = cajones.length - 1;
